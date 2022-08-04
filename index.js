1dcef0e8ba27bc52c10ddd88bae30e0075bd459b
DenisOuma <dennisouma38@gmail.com>
2022-08-04 21:18:19 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,43 @@
+// Additional function
+function add(a, b) {
+  return a + b;
+}
+
+// Subtraction function
+function subtract(a, b) {
+  return a - b;
+}
+
+// Multiplication function
+function multiply(a, b) {
+  return a * b;
+}
+
+// Division Function
+function divide(a, b) {
+  return a / b;
+}
+
+// increment by one function
+function increment(n) {
+  return n + 1;
+}
+
+// decrement with one function
+function decrement(n) {
+  return n - 1;
+}
+
+// Convert input to int
+
+function makeInt(n) {
+  let a = parseInt(n, 10);
+  return a;
+}
+
+// floating number return
+
+function preserveDecimal(n) {
+  let a = parseFloat(n);
+  return a;
+}
